6e8aa55c462ca29fa085b68068251b766d809fad
Saba <sabaazeem025@gmail.com>
2024-01-06 09:29:46 +0500

    add backend route for orders

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,10 @@
-import asyncHandldr from '../middleware/asyncHandler.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 //@desc Fetch all produts
 //@route GET /api/products
 //@access Public
-const getProducts = asyncHandldr(async(req,res)=>{
+const getProducts = asyncHandler(async(req,res)=>{
     const products=await Product.find({});
     res.json(products);
 });
@@ -12,7 +12,7 @@ const getProducts = asyncHandldr(async(req,res)=>{
 //@desc Fetch a produt
 //@route GET /api/products/:id
 //@access Public
-const getProductById = asyncHandldr(async(req,res)=>{
+const getProductById = asyncHandler(async(req,res)=>{
     const produt= await Product.findById(req.params.id);
     if(produt){
         res.json(produt);
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
+
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 
 const port=process.env.PORT || 5000;
@@ -20,6 +23,7 @@ app.use(cookieParser());
 //Routes
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
+app.use('/api/orders',orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
